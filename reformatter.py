d199ff535a047317668235baf277803d61eddc74
Lukas Cervenak <lukas.cervenak@student.tuke.sk>
2022-12-04 03:52:00 +0100

    additional scripts to manipulate jsons

--- reformatter.py
+++ reformatter.py
@@ -1,7 +1,8 @@
 import json
 import pandas as pd
+from sklearn import preprocessing as prep
 
-
+'''
 for i in range(1, 51):
     file = open('data_https/out' + str(i) + '.json')
     file_out = open('data_https/new' + str(i) + '.json', 'w')
@@ -9,10 +10,42 @@ for i in range(1, 51):
     data_nor = pd.json_normalize(data)
     data_time = data_nor['metric.timestamps'][0]
     data_val = data_nor['metric.values'][0]
+    data_val = prep.minmax_scale(data_val, feature_range=(0, 100))
 
     file_out.write('{\n"timeseries": [\n')
-    for i in range(len(data_val) - 1):
-        file_out.write('{ "value": ' + str(data_val[i]))
-        file_out.write(', "time": ' + str(data_time[i]) + ' },\n')
+    for j in range(len(data_val) - 1):
+        file_out.write('{ "value": ' + str(data_val[j]))
+        file_out.write(', "time": ' + str(data_time[j]) + ' },\n')
     file_out.write('{ "value": ' + str(data_val[len(data_val) - 1]))
     file_out.write(', "time": ' + str(data_time[len(data_val) - 1]) + ' }\n]}')
+'''
+
+file_avg = open('data_https/avg.json', 'w')
+filenames = ['data_https/out7.json', 'data_https/out2.json', 'data_https/out15.json']
+file = open(filenames[0])
+data1 = json.loads(file.read())
+data_nor1 = pd.json_normalize(data1)
+data_val1 = data_nor1['metric.values'][0]
+data_val1 = prep.minmax_scale(data_val1, feature_range=(0, 100))
+file = open(filenames[1])
+data2 = json.loads(file.read())
+data_nor2 = pd.json_normalize(data2)
+data_val2 = data_nor2['metric.values'][0]
+data_val2 = prep.minmax_scale(data_val2, feature_range=(0, 100))
+file = open(filenames[2])
+data3 = json.loads(file.read())
+data_nor3 = pd.json_normalize(data3)
+data_val3 = data_nor3['metric.values'][0]
+data_val3 = prep.minmax_scale(data_val3, feature_range=(0, 100))
+
+data_time = data_nor1['metric.timestamps'][0]
+file_avg.write('{\n"timeseries": [\n')
+for i in range(len(data_time) - 1):
+    file_avg.write('{ "time": ' + str(data_time[i]) + ', ')
+    file_avg.write('"value1": ' + str(data_val1[i]) + ', ')
+    file_avg.write('"value2": ' + str(data_val2[i]) + ', ')
+    file_avg.write('"value3": ' + str(data_val3[i]) + ' },\n')
+file_avg.write('{ "time": ' + str(data_time[len(data_time) - 1]) + ', ')
+file_avg.write('"value1": ' + str(data_val1[len(data_time) - 1]) + ', ')
+file_avg.write('"value2": ' + str(data_val2[len(data_time) - 1]) + ', ')
+file_avg.write('"value3": ' + str(data_val3[len(data_time) - 1]) + ' }\n]}')
--- test.py
+++ test.py
@@ -3,23 +3,54 @@ import json
 import pandas as pd
 from matplotlib import pyplot as plt
 import datetime
+from sklearn import preprocessing as prep
 
 
 for i in range(1, 20):
-    file = open('data/out' + str(i) + '.json')
-    data = json.loads(file.read())
-    data_nor = pd.json_normalize(data)
+    filename1 = ('data_https/out' + str(i) + '.json')
+    filename2 = ('data_bandwidth/out' + str(i) + '.json')
+    file1 = open(filename1)
+    file2 = open(filename2)
+    data1 = json.loads(file1.read())
+    data_nor1 = pd.json_normalize(data1)
+    data2 = json.loads(file2.read())
+    data_nor2 = pd.json_normalize(data2)
 
-    data_val = data_nor['metric.values'][0]
-    data_time = data_nor['metric.timestamps'][0]
+    data_val1 = data_nor1['metric.values'][0]
+    data_time1 = data_nor1['metric.timestamps'][0]
+    data_val2 = data_nor2['metric.values'][0]
+    data_time2 = data_nor2['metric.timestamps'][0]
 
-    for j in range(len(data_time)):
-        data_time[j] = datetime.datetime.fromtimestamp(data_time[j]/1000)
+    for j in range(len(data_time1)):
+        data_time1[j] = datetime.datetime.fromtimestamp(data_time1[j]/1000)
+        data_time2[j] = datetime.datetime.fromtimestamp(data_time2[j]/1000)
 
-    x = np.array(data_time)
-    y = np.array(data_val)
-    plt.title(str(data_nor['metric.code'][0]))
+    x = np.array(data_time1)
+    data_val1 = np.array(data_val1)
+    y = prep.minmax_scale(data_val1, feature_range=(0, 10))
+    plt.title(data_nor1['metric.code'][0] + ' - ' + filename1)
+    plt.xlabel("timestamps")
+    plt.ylabel("values")
+    plt.plot(x, y)
+    data_val2 = np.array(data_val2)
+    y = prep.minmax_scale(data_val2, feature_range=(0, 10))
     plt.xlabel("timestamps")
     plt.ylabel("values")
     plt.plot(x, y)
     plt.show()
+'''
+
+file = open('data_https/out1.json')
+file_out = open('data_https/new1.json', 'w')
+data = json.loads(file.read())
+data_nor = pd.json_normalize(data)
+data_time = data_nor['metric.timestamps'][0]
+data_val = data_nor['metric.values'][0]
+
+file_out.write('{\n"timeseries": [\n')
+for i in range(len(data_val) - 1):
+    file_out.write('{ "value": ' + str(data_val[i]))
+    file_out.write(', "time": ' + str(data_time[i]) + ' },\n')
+file_out.write('{ "value": ' + str(data_val[len(data_val) - 1]))
+file_out.write(', "time": ' + str(data_time[len(data_val) - 1]) + ' }\n]}')
+'''
